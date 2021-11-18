3b68ba8657ead9a7b65cea9b3d00f5767018eabb
DiSarD <disard01@gmail.com>
2021-11-18 23:11:14 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,6 +7,17 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
@@ -18,15 +29,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String name) {
@@ -41,5 +45,15 @@ public class Tracker {
         }
         Item[] rsl = Arrays.copyOf(tempArray, size);
         return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int idx = indexOf(id);
+        if (idx != -1) {
+            items[idx].setName(item.getName());
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -37,6 +37,19 @@ import static org.junit.Assert.assertThat;
         }
 
         @Test
+        public void whenReplace() {
+            Tracker tracker = new Tracker();
+            Item bug = new Item();
+            bug.setName("Bug");
+            tracker.add(bug);
+            int id = bug.getId();
+            Item bugWithDesc = new Item();
+            bugWithDesc.setName("Bug with description");
+            tracker.replace(id, bugWithDesc);
+            assertThat(tracker.findById(id).getName(), is("Bug with description"));
+        }
+
+        @Test
         public void whenTestFindByNameCheckArrayLength() {
             Tracker tracker = new Tracker();
             Item first = new Item("First");
