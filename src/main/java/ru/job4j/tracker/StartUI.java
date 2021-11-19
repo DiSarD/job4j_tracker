222d1285a0a81ff0e6d3f24a33e4bbf900794226
DiSarD <disard01@gmail.com>
2021-11-19 14:21:35 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,7 +4,8 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item(123, "Anton");
+        System.out.println(item);
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(dateFormat));
     }
