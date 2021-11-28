86af652aab5c32e33d4ae02ea8d029fcc98b552f
DiSarD <disard01@gmail.com>
2021-11-28 18:36:57 +0300

    Исправление 4.1. Разрыв зависимости StartUI от Scanner. [#181778 #233776]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,9 @@ package ru.job4j.tracker;
 public class StartUI {
 
     public void init(Input input, Tracker tracker) {
+
         boolean run = true;
+
         while (run) {
             showMenu();
             System.out.print("Select: ");
