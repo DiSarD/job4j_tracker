f3a546619ca4058c49e99dbdb3613ad10231020e
DiSarD <disard01@gmail.com>
2021-12-15 00:06:18 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class FactTest  {
 
     @Test (expected = IllegalArgumentException.class)
-    public void WhenNumberIsNegative() {
+    public void whenNumberIsNegative() {
         new Fact().calc(-1);
     }
 }
